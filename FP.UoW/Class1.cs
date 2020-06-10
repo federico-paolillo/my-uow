b1700c48ed5588819254c92e513352cdcd8b64f8
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2020-06-10 17:00:46 +0200

    Configured main project ruleset and added deps.

--- FP.UoW/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FP.UoW
-{
-    public class Class1
-    {
-    }
-}
---
+++ FP.UoW/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds an Unit of Work to the <see cref="IServiceCollection"/> specified, optionally using the <see cref="ServiceLifetime"/> provided.
+        /// By default the Unit of Work will use a <see cref="ServiceLifetime.Scoped"/> lifetime.
+        /// </summary>
+        public static IServiceCollection AddUoW(this IServiceCollection services, ServiceLifetime uowLifetime = ServiceLifetime.Scoped)
+        {
+            if (services is null) throw new ArgumentNullException(nameof(services));
+
+            return services;
+        }
+    }
+}
